cd5e912563cb8579fdd447981eac1e789f0d7509
bababa <pijonsan@gmail.com>
2026-01-20 23:24:28 +0900

    fix 変数名

--- terraform/proxmox-vms/provider.tf
+++ terraform/proxmox-vms/provider.tf
@@ -1,10 +1,10 @@
 provider "proxmox" {
   # endpoint は variables.tf の default で決まる
-  endpoint = var.proxmox_endpoint
+  endpoint = var.endpoint
 
   # token は env(PROXMOX_VE_API_TOKEN) があれば provider 側が拾えるので、
   # var を使わず空でもOKにしておくのが事故りにくい
-  api_token = try(var.proxmox_api_token, null)
+  api_token = try(var.api_token, null)
 
   insecure = true
 }
