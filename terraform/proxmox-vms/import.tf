b5cf36e89efc2b637acb8e3d64ad3cd2a02fe604
bababa <pijonsan@gmail.com>
2026-02-07 18:02:18 +0900

    add import

--- terraform/proxmox-vms/import.tf
+++ terraform/proxmox-vms/import.tf
@@ -2,3 +2,31 @@
 #  to = proxmox_virtual_environment_vm.ubuntu_temp
 #  id = "proxmox-host1/9000"
 #}
+import {
+  to = proxmox_virtual_environment_vm.ubuntu_temp
+  id = "proxmox-host1/9000"
+}
+import {
+  to = proxmox_virtual_environment_vm.kube_ctrl
+  id = "proxmox-host1/100"
+}
+import {
+  to = proxmox_virtual_environment_vm.kube_worker_ubuntu
+  id = "proxmox-host1/105"
+}
+import{
+  to = proxmox_virtual_environment_vm.kube_worker_rhel
+  id = "proxmox-host1/101"
+}
+import{
+  to = proxmox_virtual_environment_vm.truenas
+  id = "proxmox-host1/102"
+}
+import{
+  to = proxmox_virtual_environment_vm.nfs_server
+  id = "proxmox-host1/103"
+}
+import{
+  to = proxmox_virtual_environment_vm.test_server
+  id = "proxmox-host1/107"
+}
