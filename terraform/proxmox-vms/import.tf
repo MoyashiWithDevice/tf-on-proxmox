17b272b4b3a50bb6c3a32184d4b3ae7ca7d7afd7
bababa <pijonsan@gmail.com>
2026-02-08 15:31:44 +0900

    fx

--- terraform/proxmox-vms/import.tf
+++ terraform/proxmox-vms/import.tf
@@ -1,7 +1,3 @@
-#import {
-#  to = proxmox_virtual_environment_vm.ubuntu_temp
-#  id = "proxmox-host1/9000"
-#}
 import {
   to = proxmox_virtual_environment_vm.ubuntu_temp
   id = "proxmox-host1/9000"
