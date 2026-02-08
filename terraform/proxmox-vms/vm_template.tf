c238cbb55b5592c023b337571e654a50fbaf90ef
bababa <pijonsan@gmail.com>
2026-02-08 15:00:03 +0900

    add template

--- terraform/proxmox-vms/vm_template.tf
+++ terraform/proxmox-vms/vm_template.tf
@@ -6,6 +6,14 @@ resource "proxmox_virtual_environment_vm" "ubuntu_temp" {
   tags                                 = ["template"]
   template                             = true
   vm_id                                = 9000
+  
+  initialization {
+    datastore_id = "local-lvm"
+    interface = "ide2"
+    
+    user_data_file_id = proxmox_virtual_environment_file.cloud_config.id
+  }
+  wait_for_network_config = true
   agent{
     enabled = true
   }
